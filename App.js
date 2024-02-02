d2ef0f23cbb4bfb7b24641dc338d7548d49fbae5
Dan Desktop <d95tan@gmail.com>
2024-02-02 15:50:46 +0800

    feat: first commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count+1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Hello there</Text>
+      <Text>Counter = {count}</Text>
+      <Button title="button1" onPress={handleClick}>Click me</Button>
       <StatusBar style="auto" />
     </View>
   );
