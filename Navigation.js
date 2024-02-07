8ff026d4b84610bb8b1e791189b5d4448ff774b1
Dan Desktop <d95tan@gmail.com>
2024-02-07 10:03:20 +0800

    feat: tab navigation once logged in

--- Navigation.js
+++ Navigation.js
@@ -1,16 +1,31 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/screens/HomeScreen";
 import TripsScreen from "./src/screens/TripsScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import SignupScreen from "./src/screens/SignupScreen";
+import PlanningScreen from "./src/screens/PlanningScreen";
+import NewTripScreen from "./src/screens/NewTripScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function Navigation({ user, setUser }) {
+const Navigation = ({ user, setUser }) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={user ? "Login" : "Home"}>
+      <Stack.Navigator initialRouteName={user ? "Trips" : "Home"}>
+        <Stack.Screen
+          name="Trips"
+          component={TripsScreen}
+          options={{ title: `Welcome back, ${user.email}` }}
+        />
+        <Stack.Screen
+          name="NewTrip"
+          component={NewTripScreen}
+          options={{ title: `New Trip` }}
+        />
+        <Stack.Screen name="SingleTrip" component={SingleTripNavigator} />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -28,12 +43,31 @@ export default function Navigation({ user, setUser }) {
           options={{ title: "Log In" }}
           initialParams={{ setUser }}
         />
-        <Stack.Screen
-          name="Trips"
-          component={TripsScreen}
-          initialParams={{ user, setUser }}
-        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+const SingleTripNavigator = () => {
+  return (
+    <Tab.Navigator initialRouteName="Planning">
+      <Tab.Screen
+        name="Planning"
+        component={PlanningScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Expenses"
+        component={PlanningScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Whatever"
+        component={PlanningScreen}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Navigation;
