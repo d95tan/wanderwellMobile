d1d43b6e59aad46d2c5158da450e93c9f779e097
Dan Desktop <d95tan@gmail.com>
2024-02-08 17:28:56 +0800

    refactor: extracted sendRequest to external file

--- src/utilities/trips/tripsAPI.js
+++ src/utilities/trips/tripsAPI.js
@@ -1,5 +1,5 @@
 import { API_BASE_URL } from "../../../config";
-import { getToken } from "../users/getToken";
+import sendRequest from "../sendAPIRequest";
 
 const baseURL = API_BASE_URL + "/trips";
 
@@ -9,28 +9,4 @@ export async function createTrip(body) {
 
 export async function getTrips() {
   return sendRequest(baseURL, "GET")
-}
-
-//* Template function for sending requests
-async function sendRequest(url, method = "GET", payload = null) {
-  const options = { method };
-  if (payload) {
-    options.headers = { "Content-Type": "application/json" };
-    options.body = JSON.stringify(payload);
-  }
-
-  const token = await getToken();
-
-  // console.log("sending token:", token)
-  if (token) {
-    options.headers ||= {};
-    options.headers.Authorization = `Bearer ${token}`;
-  } else {
-    throw new Error("No token found");
-  }
-
-  const res = await fetch(url, options);
-
-  if (res.ok) return res.json();
-  throw new Error("Bad Request");
 }
--- src/utilities/users/usersAPI.js
+++ src/utilities/users/usersAPI.js
@@ -1,5 +1,5 @@
 import { API_BASE_URL } from "../../../config.js";
-import { getToken } from "./getToken.js";
+import sendRequest from "../sendAPIRequest.js";
 
 const baseURL = API_BASE_URL + "/users";
 
@@ -9,25 +9,4 @@ export async function signUp(body) {
 
 export async function logIn(body) {
   return sendRequest(baseURL + "/login", "POST", body);
-}
-
-//* Template function for sending requests
-async function sendRequest(url, method = "GET", payload = null) {
-  const options = { method };
-  if (payload) {
-    options.headers = { "Content-Type": "application/json" };
-    options.body = JSON.stringify(payload);
-  }
-
-  const token = await getToken();
-
-  if (token) {
-    options.headers ||= {};
-    options.headers.Authorization = `Bearer ${token}`;
-  }
-
-  const res = await fetch(url, options);
-
-  if (res.ok) return res.json();
-  throw new Error("Bad Request");
 }
